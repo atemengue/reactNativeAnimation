dbd615802cf82a3fafffe77dcdf197e8a728988a
atemengue <moafembe@gmail.com>
2019-05-16 09:52:32 +0100

    translate Video2 ok

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Animated, TouchableWithoutFeedback } from 'react-native';
 
 class App extends React.Component {
+  state = {
+    animation: new Animated.Value(0)
+  }
+  startAnimation = () => {
+    Animated.timing(this.state.animation,{
+      toValue: -300,
+      duration: 1500
+    }).start(() => this.state.animation.setValue(0))
+  }
   render() {
+    const animatedStyles = {
+      transform: [
+        {
+          translateY: this.state.animation
+        }
+      ]
+    }
     return(
-      <View style={styles.container}>
-        <Text>React Native Animation</Text>
+      <View style={styles.container }>
+        <TouchableWithoutFeedback onPress={this.startAnimation}>
+          <Animated.View style={[styles.box, animatedStyles]}/>
+        </TouchableWithoutFeedback>
       </View>
     )
   }
@@ -16,6 +34,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  box: {
+    height: 150,
+    width: 150,
+    backgroundColor: 'tomato'
   }
 })
 
