6d74382fd17c00f65b4bc2f8242cab142e6e245f
atemengue <moafembe@gmail.com>
2019-05-16 10:50:52 +0100

    video 5 animated AbsolutePosition OK

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet, Animated, TouchableWithoutFeedback } from 'react-native';
 
 class App extends React.Component {
+  state = {
+    animation: new Animated.Value(0)
+  }
+
+  startAnimation = () => {
+    Animated.timing(this.state.animation, {
+      toValue: 40,
+      duration: 1500
+    }).start(() => {
+      Animated.timing(this.state.animation, {
+        toValue: 0,
+        duration: 500
+      }).start();
+    });
+  }
   render() {
+    const animationStyles = {
+      top: this.state.animation,
+      left: this.state.animation,
+      right: this.state.animation
+    }
     return(
       <View style={styles.container}>
-        <Text>React Native Animation</Text>
+        <TouchableWithoutFeedback onPress={this.startAnimation}>
+          <Animated.View style={[styles.box, animationStyles]} />
+        </TouchableWithoutFeedback>
       </View>
     )
   }
@@ -16,6 +38,14 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  box: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    height: 100,
+    backgroundColor: 'tomato'
   }
 })
 
