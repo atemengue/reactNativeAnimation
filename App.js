6402d58a8bc1da1812726489e7284e569481018e
atemengue <moafembe@gmail.com>
2019-05-16 12:01:52 +0100

    video 7 timing Rotation

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Animated, TouchableWithoutFeedback } from 'react-native';
 
 class App extends React.Component {
+  state = {
+    animation: new Animated.Value(0)
+  }
+
+  startAnimation = () => {
+    Animated.timing(this.state.animation,{
+      toValue: 360,
+      duration: 1500
+    }).start();
+  }
+
+
   render() {
+    const rotateInterpolate = this.state.animation.interpolate({
+      inputRange: [0, 360],
+      outputRange:['0deg', '360deg']
+    })
+
+    const rotateAnimationStyle = {
+      transform: [
+        {
+          rotate: rotateInterpolate
+        }
+      ]
+    }
     return(
       <View style={styles.container}>
-        <Text>React Native Animation</Text>
+        <TouchableWithoutFeedback onPress={this.startAnimation}>
+          <Animated.View style={[styles.box, rotateAnimationStyle]}>
+            <Text>React Native Animation</Text>
+          </Animated.View>
+        </TouchableWithoutFeedback>
       </View>
     )
   }
@@ -16,6 +44,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  box: {
+    width: 150,
+    height: 150,
+    backgroundColor: 'tomato'
   }
 })
 
