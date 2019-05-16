3639a2cb8f2cf268c079a24679623a02ec150418
atemengue <moafembe@gmail.com>
2019-05-16 14:22:15 +0100

    Video 8 heightWidthPercentage

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet, Animated, TouchableWithoutFeedback } from 'react-native';
 
 class App extends React.Component {
+  state = {
+    animation: new Animated.Value(0)
+  }
+
+  startAnimation = () => {
+    Animated.timing(this.state.animation, {
+      toValue: 1,
+      duration: 1500
+    }).start()
+  }
+  
   render() {
+    const heightInterpolation = this.state.animation.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['20%', '30%']
+    });
+    
+    const widthInterpolation = this.state.animation.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['20%', '50%']
+    });
+
+    const animationStyles = {
+      height: heightInterpolation,
+      width: widthInterpolation
+    }
     return(
       <View style={styles.container}>
-        <Text>React Native Animation</Text>
+        <TouchableWithoutFeedback onPress={this.startAnimation}>
+          <Animated.View style={[styles.box, animationStyles]} /> 
+        </TouchableWithoutFeedback>
       </View>
     )
   }
@@ -16,6 +43,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  box: {
+    width: '20%',
+    height: '20%',
+    backgroundColor: 'tomato'
   }
 })
 
