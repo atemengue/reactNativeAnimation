2992b22f81d7186fdc8083c078e737c9921771de
atemengue <moafembe@gmail.com>
2019-05-16 15:41:16 +0100

    Spring Animation Video 10

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet, Animated, TouchableWithoutFeedback } from 'react-native';
 
 class App extends React.Component {
+  state = {
+    animation: new Animated.Value(1)
+  }
+  
+  startAnimation = () => {
+    this.state.animation.addListener((value)=> {
+      console.log(value);
+    })
+    Animated.spring(this.state.animation, {
+      toValue: 2,
+      friction: 20,
+      tension: 180
+    }).start(() => {
+      Animated.timing(this.state.animation, {
+        toValue: 1,
+        duration: 300
+      }).start()
+    })
+  }
   render() {
+    const animationStyle = {
+      transform: [{ scale: this.state.animation }]
+    }
     return(
       <View style={styles.container}>
-        <Text>React Native Animation</Text>
+        <TouchableWithoutFeedback onPress={this.startAnimation}>
+          <Animated.View style={[styles.box, animationStyle]} />
+        </TouchableWithoutFeedback>
       </View>
     )
   }
@@ -16,6 +40,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  box: {
+    height: 80,
+    width: 80,
+    backgroundColor: 'tomato'
   }
 })
 
