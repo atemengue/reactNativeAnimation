ca2273135e8456c3c9d87f9895e7399ab020fa57
atemengue <moafembe@gmail.com>
2019-05-16 10:23:12 +0100

    video 4 heightWidth

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Animated,TouchableWithoutFeedback } from 'react-native';
 
 class App extends React.Component {
+  state = {
+    animation: new Animated.Value(150)
+  }
+
+  startAnimation = () => {
+    Animated.timing(this.state.animation, {
+      toValue: 300,
+      duration: 500
+    }).start()
+  }
   render() {
+    const animationStyles = {
+      height: this.state.animation,
+      width: this.state.animation
+    }
     return(
       <View style={styles.container}>
-        <Text>React Native Animation</Text>
+      <TouchableWithoutFeedback onPress={this.startAnimation}>
+        <Animated.View style={[styles.box, animationStyles]} />
+      </TouchableWithoutFeedback>
       </View>
     )
   }
@@ -16,6 +32,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  box: {
+    width: 150,
+    backgroundColor: 'tomato'
   }
 })
 
