f136f93fce6a7356cd4bfa56f3382ff457588ee5
atemengue <moafembe@gmail.com>
2019-05-16 15:18:03 +0100

    video 9 Easing OK

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Animated, TouchableWithoutFeedback, Easing } from 'react-native';
 
 class App extends React.Component {
+  state = {
+    animation: new Animated.Value(0)
+  }
+
+  startAnimation = () => {
+    Animated.timing(this.state.animation, {
+      toValue: 300,
+      duration: 500,
+      easing: Easing.bounce,
+      // easing: Easing.back(5),
+      // easing: Easing.elastic(3),
+      // easing: Easing.bezier(.06, 1, .86, .23)
+    }).start()
+  }
   render() {
+    const animationStyle = {
+      transform: [{ translateY: this.state.animation }]
+    }
     return(
       <View style={styles.container}>
-        <Text>React Native Animation</Text>
+      <TouchableWithoutFeedback onPress={this.startAnimation}>
+        <Animated.View style={[styles.box, animationStyle]} />
+      </TouchableWithoutFeedback>
       </View>
     )
   }
@@ -16,8 +35,15 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  box: {
+    height: 100,
+    width: 100,
+    position: 'relative',
+    left: 0,
+    backgroundColor: 'tomato'
   }
-})
+});
 
 
 export default App;
