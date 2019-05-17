14e92f64f85f4ddec238a8c3212d370f4208db40
atemengue <moafembe@gmail.com>
2019-05-17 09:37:20 +0100

    decay video 11 OK

--- App.js
+++ App.js
@@ -1,11 +1,56 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, PanResponder, Animated } from 'react-native';
 
 class App extends React.Component {
+  state = {
+    animation: new Animated.ValueXY(0)
+  }
+
+  componentWillMount() {
+
+    // this.state.animation.addListener((value) => {
+    //   this._x = value.x,
+    //   this._y = value.y
+    // })
+
+    this._panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onMoveShouldSetPanResponder: () => true,
+      // onPanResponderGrant: () => {
+      //   this.state.animation.setOffset({
+      //     x: this._x,
+      //     y: this._y
+      //   });
+      //   this.state.animation.setValue({
+      //     x: 0,
+      //     y: 0
+      //   })
+      // },
+
+      onPanResponderGrant: () => {
+        this.state.animation.extractOffset()
+      },
+      onPanResponderMove: Animated.event([
+        null, {
+          dx: this.state.animation.x,
+          dy: this.state.animation.y
+        }
+      ]),
+      onPanResponderRelease: (evt, { vx, vy}) => {
+        Animated.decay(this.state.animation,{
+          velocity: {x: vx, y: vy },
+          deceleration: 0.997
+        }).start()
+      }
+    })
+  }
   render() {
+    const dStyle = {
+      transform: this.state.animation.getTranslateTransform()
+    }
     return(
       <View style={styles.container}>
-        <Text>React Native Animation</Text>
+        <Animated.View style={[styles.box, dStyle]} { ...this._panResponder.panHandlers} />
       </View>
     )
   }
@@ -16,6 +61,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  box: {
+    height: 80,
+    width: 80,
+    backgroundColor: 'tomato'
   }
 })
 
