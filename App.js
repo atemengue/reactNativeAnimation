5d95baaae32462ca3792b2f3843c797889c0a060
atemengue <moafembe@gmail.com>
2019-05-17 09:05:15 +0100

    event scrollview Video11

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Animated } from 'react-native';
 
 class App extends React.Component {
+  state = {
+    animation: new Animated.Value(0)
+  }
   render() {
+    const backInterpolate = this.state.animation.interpolate({
+      inputRange: [0, 3000],
+      outputRange: ['rgb(145, 15, 200)', 'rgb(200, 15, 145)']
+    });
+
+    const backgroundStyle = {
+      backgroundColor: backInterpolate
+    }
     return(
       <View style={styles.container}>
-        <Text>React Native Animation</Text>
+        <ScrollView onScroll={Animated.event([{
+          nativeEvent: {
+            contentOffset: {
+              y : this.state.animation
+            }
+          }
+          }
+        ])} scrollEventThrottle={16}>
+          <Animated.View style={[styles.content, backgroundStyle]} />
+        </ScrollView>
       </View>
     )
   }
@@ -14,10 +34,12 @@ class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+  },
+  content: {
+    height: 3000
   }
 })
+
 
 
 export default App;
