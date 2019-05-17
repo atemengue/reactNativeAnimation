beb0db56aeb3d31989cf0b622bf6ed55b85549c0
atemengue <moafembe@gmail.com>
2019-05-17 10:03:08 +0100

    video13 OK

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet, Animated, TouchableWithoutFeedback } from 'react-native';
 
 class App extends React.Component {
+  state = {
+    animation: new Animated.Value(0)
+  }
+  startAnimation = () => {
+    Animated.timing(this.state.animation, {
+      toValue: 300,
+      duration: 1500
+    }).start(() => {
+      Animated.timing(this.state.animation, {
+        toValue: 0,
+        duration: 500
+      }).start();
+    })
+  }
   render() {
+    const randomValue = new Animated.Value(50)
+    const newAnimation = Animated.add(
+      this.state.animation, randomValue
+    )
+    const animationOne = {
+      transform: [{ translateY: newAnimation }]
+    }
+
     return(
       <View style={styles.container}>
-        <Text>React Native Animation</Text>
+        <TouchableWithoutFeedback onPress={this.startAnimation}>
+          <Animated.View style={[styles.box, animationOne]} />
+        </TouchableWithoutFeedback>
       </View>
     )
   }
@@ -16,7 +40,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
-  }
+    },
+    box: {
+      height: 80,
+      width: 80,
+      backgroundColor: 'tomato'
+    }
 })
 
 
